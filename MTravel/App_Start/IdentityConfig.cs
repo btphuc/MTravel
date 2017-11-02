d863c4f13e55dfbf8bdb88c12e0dcbd6c32fa3a7
btphuc <btphuc.eznw@gmail.com>
2017-11-02 14:23:23 +0700

    init

--- MTravel/App_Start/IdentityConfig.cs
+++ MTravel/App_Start/IdentityConfig.cs
@@ -53,11 +53,11 @@ namespace MTravel
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
